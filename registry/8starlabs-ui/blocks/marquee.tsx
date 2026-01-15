5a3db09b1b1b76a226cd21d6b70b69d93bc1ca84
Larry Lee <larrylee2003@live.com>
2026-01-15 20:26:44 +0800

    feat: add Marquee component

---
+++ registry/8starlabs-ui/blocks/marquee.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+import React from "react";
+
+interface MarqueeProps {
+  greyscale?: boolean;
+  direction?: "left" | "right";
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export default function Marquee({
+  greyscale = false,
+  direction = "left",
+  children,
+  className
+}: MarqueeProps) {
+  const content = React.Children.toArray(children);
+  const repeat = 4;
+  const duplicatedContent = React.useMemo(() => {
+    if (content.length === 0) {
+      return [];
+    } else if (content.length < repeat) {
+      return Array.from({ length: repeat }, () => content).flat();
+    }
+    return content;
+  }, [content, repeat]);
+  const animationClass =
+    direction === "left" ? "animate-marquee" : "animate-marquee-reverse";
+
+  return (
+    <div
+      className={cn(
+        "group/marquee flex overflow-hidden p-2 [--gap:1rem]",
+        greyscale && "grayscale",
+        className
+      )}
+    >
+      <div
+        className={cn(
+          "flex shrink-0 min-w-full items-center gap-[var(--gap)] pr-[var(--gap)] group-hover/marquee:[animation-play-state:paused]",
+          animationClass
+        )}
+      >
+        {duplicatedContent.length === 0 ? (
+          <>
+            <div
+              aria-hidden="true"
+              className="bg-black rounded-xl h-20 w-40"
+            ></div>
+            <div className="bg-black rounded-xl h-20 w-40"></div>
+            <div className="bg-black rounded-xl h-20 w-40"></div>
+          </>
+        ) : (
+          duplicatedContent.map((item, index) => (
+            <div key={`source-${index}`}>{item}</div>
+          ))
+        )}
+      </div>
+
+      <div
+        className={cn(
+          "flex shrink-0 min-w-full items-center gap-[var(--gap)] pr-[var(--gap)] group-hover/marquee:[animation-play-state:paused]",
+          animationClass
+        )}
+      >
+        {duplicatedContent.length === 0 ? (
+          <>
+            <div
+              aria-hidden="true"
+              className="bg-black rounded-xl h-20 w-40"
+            ></div>
+            <div className="bg-black rounded-xl h-20 w-40"></div>
+            <div className="bg-black rounded-xl h-20 w-40"></div>
+          </>
+        ) : (
+          duplicatedContent.map((item, index) => (
+            <div key={`source-${index}`}>{item}</div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
